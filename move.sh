4e2b476d3b76b5159e4c10c2b58ae1417fd58e63
Gaurav Singh <gauravnumber@gmail.com>
2022-06-02 21:21:34 +0530

    dir to dir transfer possible

--- move.sh
+++ move.sh
@@ -2,36 +2,64 @@ moveSingleFileToDirectory() {
 	file="$1"
 	dir="$2"
 
-	# If destination file not exist then created
-	if [[ ! -e "$dir/$file" ]]; then
-		mv -n "$file" "$dir/$file"
-		exit 0
-	fi
+	# if [[ -d $file ]]; then echo "dire"; fi
+
+	#? If destination file not exist then created
+	# if [[ ! -e "$dir/$file" ]]; then
+	# 	# echo "hie"
+	# 	echo '$dir/$file' "$dir/$file"
+
+	# 	mv -n "$file" "$dir/$file"
+	# 	exit 0
+	# fi
 
 	index=1
 
-	for i in "$dir"/*; do
+	for destinationFile in "$dir"/*; do
 		newfileName=$file
+		#? rm src. dir name
+		#? foo/1.txt => 1.txt
+		newfileName=${newfileName/[a-z]*\//}
+		# newfileName=${newfileName/$dir\//}
 
-		while [[ "$i" == "$dir/$newfileName" ]]; do
-			newfileName=$(echo "$i" | sed -E "s|(\w+)\.(\w+)|\1_$index\.\2|g;")
-			newfileName=${newfileName/$dir\//}
+		# echo '$newfileName' "$newfileName"
 
+		#? rm dest. dir name
+		#? bar/1.txt => 1.txt
+		destinationFile=${destinationFile/$dir\//}
+		# echo '$destinationFile' "$destinationFile"
+		# echo "$dir"/*
+		# while [[ "$destinationFile" == "$dir/$newfileName" ]]; do
+		while [[ "$destinationFile" == "$newfileName" ]]; do
+			newfileName=$(echo "$destinationFile" | sed -E "s|(\w+)\.(\w+)|\1_$index\.\2|g;")
+			# newfileName=${newfileName/$dir\//}
+			# echo '$newfileName' "$newfileName"
+
+			# while [[ -e "$newfileName" ]]; do
 			while [[ -e "$dir/$newfileName" ]]; do
 				index=$(($index + 1))
-				newfileName=$(echo "$i" | sed -E "s|(\w+)\.(\w+)|\1_$index\.\2|g;")
-				newfileName=${newfileName/$dir\//}
+				newfileName=$(echo "$destinationFile" | sed -E "s|(\w+)\.(\w+)|\1_$index\.\2|g;")
+				# newfileName=${newfileName/$dir\//}
 			done
 
+			# echo '$newfileName' "$newfileName"
 			mv -n "$1" "$dir/$newfileName"
 		done
 	done
 
-	echo "ihe"
 }
 
 moveAllFilesOfDirectoryToAnotherDirectory() {
-	echo "directory"
+	# echo "$2"
+	for file in "$1"/*; do
+		# echo ${file/$1\//}
+		# ? foo/1.txt => 1.txt
+		# file=${file/$1\//}
+		# echo "$file"
+		moveSingleFileToDirectory "$file" "$2"
+	done
+
+	# echo "directory"
 
 }
 
